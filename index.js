2e45b8a6b4647a5133e3de6ab5f2b0eeb0c2da7c
Aashutosh Joshi <125844407+ashutoshjoshi47@users.noreply.github.com>
2026-07-23 11:17:08 +0545

    vectors compared by cosine similarity

--- index.js
+++ index.js
@@ -22,6 +22,14 @@ async function createEmbedding(text) {
     return response.embeddings[0].values;
 }
 
+function cosineSimilarity(vecA, vecB){
+    let dotProduct = 0;
+    for(let i=0; i<vecA.length ; i++){
+        dotProduct+= vecA[i]*vecB[i]
+    }
+    return dotProduct;
+}
+
 app.get('/', (req, res) => {
     res.send("API is working")
 })
@@ -35,27 +43,43 @@ app.post('/upload', upload.single("pdf"), async (req, res) => {
         const text = pdfData.text;
 
         const chunks = text.split('\n\n').filter((chunk)=> chunk.trim() != '');
-        console.log(chunks);
 
-        const embedding = await createEmbedding(chunks[0]);
-        console.log(embedding);
+        const chunkEmbeddings = [];
+
+        for(const chunk of chunks){
+            const embedding = await createEmbedding(chunk);
+
+            chunkEmbeddings.push({
+                text: chunk,
+                embedding
+            })
+        }
+        
 
         const question = req.body.question;
-        console.log(question)
-        const matchedChunk = chunks.find((chunk) => chunk.toLowerCase().includes(question));
+        const questionEmbedding = await createEmbedding(question);
+
+        // const matchedChunk = chunks.find((chunk) => chunk.toLowerCase().includes(question));
+
+        let bestChunk = 0;
+        let bestScore = -Infinity;
+
+        for(const items of chunkEmbeddings){
+            const score = cosineSimilarity(questionEmbedding, items.embedding);
+            if(score > bestScore){
+                bestChunk = items.text;
+                bestScore = score;
+            }
+        }
+        console.log(bestScore);
 
         const response = await ai.models.generateContent({
             model: 'gemini-2.5-flash-lite',
-            contents: `Answer the question using the context: ${matchedChunk}
+            contents: `Answer the question using the context: ${bestChunk}
             Question: ${question}`
         })
 
         res.send(response.text);
-        // res.json({
-        //     matchedChunk,
-        //     response: response.text
-        // })
-
 
     } catch (err) {
         console.log(err);
